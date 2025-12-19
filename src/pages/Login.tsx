2890ac2194905b1a2701a9f44c06ba0c5bfdb01f
Eduarda Guimarães <eduardaguima2007@yahoo.com>
2025-12-19 19:54:14 -0300

    login responsivo

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,13 +4,16 @@ import { LoginForm } from "../components/LoginForm";
 
 export function Login() {
   return (
-    <main className="flex min-h-screen w-full overflow-hidden">
-      <section className="w-1/2 flex items-center justify-center">
+    <main className="flex flex-col lg:flex-row min-h-screen w-full overflow-x-hidden bg-[#F0F2F5] lg:bg-transparent">
+      
+      <section className="hidden lg:flex lg:w-1/2 items-center justify-center">
         <SideLogo />
       </section>
 
-      <section className="w-1/2 flex items-center justify-center bg-white">
-        <LoginForm />
+      <section className="w-full lg:w-1/2 flex items-center justify-center bg-white min-h-screen shadow-2xl lg:shadow-none">
+        <div className="w-full max-w-[450px] px-6">
+          <LoginForm />
+        </div>
       </section>
     </main>
   );
